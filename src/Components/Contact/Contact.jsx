9e5d9720079f45ae6e7da195d676049afade4243
mahmadasghar <146083682+mahmadasghar@users.noreply.github.com>
2024-09-03 23:31:16 +0500

    upload portfolio

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+const Contact = () => {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "ff7d3bce-f505-4f0b-8fc1-aa274424bfe0");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+         alert(res.message)
+        }
+      };
+  return (
+    <div id='contact' className='contact'>
+        <div className="contact-title">
+            <h1>Get in touch</h1>
+            <img src={theme_pattern} alt="" />
+        </div>
+        <div className="contact-section">
+            <div className="contact-left">
+                <h1>Let's talk</h1>
+                <p>I'm currently avaliable to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+                <div className="contact-details">
+                    <div className="contactdetail">
+                        <img src={mail_icon} alt="" /><p>alex123@gmail.com</p>
+                    </div>
+                  <div className="contactdetail">
+                        <img src={call_icon} alt="" /><p>+44 123 4445 5566</p>
+                    </div>
+                    <div className="contactdetail">
+                        <img src={location_icon} alt="" /><p>US , USA</p>
+                    </div>
+                </div>
+            </div>
+            <form onSubmit={onSubmit} className="contact-right">
+                <label htmlFor="">Your Name</label>
+                <input type="text" placeholder='Enter your name' name='name'/>
+                <label htmlFor="">Your Email</label>
+                <input type="email" placeholder='Enter your email' name='email' />
+                <label htmlFor="">Write your message here</label>
+                <textarea name="message" rows={8} placeholder='Enter your message'></textarea>
+                <button type='submit' className="contact-submit">Submit now</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
